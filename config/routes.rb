74c51831e2e85d3272d5ffe3dd079450000fbd95
eamesa <eamadorm@chicagobooth.edu>
2022-04-19 16:45:25 +0000

    created the partial for photo

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   resources :photos
 
   get ":username/liked" => "photos#liked", as: :liked_photos
+  # get ":username/feed" => "photos#liked", as: :liked_photos
+  # get ":username/followers" => "photos#liked", as: :liked_photos
+  # get ":username/following" => "photos#liked", as: :liked_photos
   get "/:username" => "users#show", as: :user
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
