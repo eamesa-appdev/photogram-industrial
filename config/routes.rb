186fa4b066281b673bb8588f72c70a44542446dc
eamesa <eamadorm@chicagobooth.edu>
2022-04-19 16:35:01 +0000

    creating a partial fo photo

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   resources :likes
   resources :photos
 
-  get "/:username" => "users#show"
+  get ":username/liked" => "photos#liked", as: :liked_photos
+  get "/:username" => "users#show", as: :user
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
