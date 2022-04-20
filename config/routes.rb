4141a756db1f6f424122733a591a6898970ca249
eamesa <97650612+eamesa@users.noreply.github.com>
2022-04-20 08:31:25 -0500

    Tabbed interface - Final Photogram 1
    * This commit goes from validations to adding the sample data tasks.
    
    added validations
    
    added scopes
    
    Sample data task created
    
    Sample data task finished
    
    * updating the navbar
    
    * Navbar is done
    
    * added sample users and fixed navbar
    
    * added notices and alerts
    
    * done with basic UI
    
    * fixed the sample data task after adding the example users
    
    * created users#show route
    
    * created users#show action and view
    
    * editing the user#show with image cards and css
    
    * included cards with photo, owner and commentss
    
    * included a media opbjectwith author
    
    * fixing the form to add new comment
    
    * the new comment works and now redirects to the photo show instead of a generic new comment page
    
    * the new comment works and now redirects_back with fallback
    
    * changed the route to /username
    
    * creating a partial fo photo
    
    * created the partial for photo
    
    * feed done
    
    * started to redo the navigation and UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
 
-  resources :likes
-  resources :follow_requests
-  resources :comments
-  resources :photos
-  devise_for :users
   root "photos#index"
+
+  
+  devise_for :users
+  
+  resources :comments
+  resources :follow_requests
+  resources :likes
+  resources :photos
+
+  get ":username/liked" => "photos#liked", as: :liked_photos
+  get ":username/feed" => "photos#feed", as: :feed_photos
+  # get ":username/followers" => "photos#liked", as: :liked_photos
+  # get ":username/following" => "photos#liked", as: :liked_photos
+  get "/:username" => "users#show", as: :user
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
